0a305755d9cec0061ab9fd07b153f5ed98463fb2
keidartomusa <86793181+keidartomusa@users.noreply.github.com>
2026-09-22 11:53:47 +0300

    build: add src/agent.js

---
+++ src/agent.js
@@ -0,0 +1,3 @@
+import { parseIntent } from "./intent.js";
+import { findAvailability, formatHebrew } from "./availability.js";
+export async function answer(text, opts={}) { const intent=await parseIntent(text,opts.now,opts.fetchImpl); return formatHebrew(await findAvailability(intent,opts)); }
